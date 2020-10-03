ee59b97fce7f43cf6252f557a0703afe6a8c9d28
jaquesloh <jaquesloh@gmail.com>
2020-10-03 03:38:10 +0300

    feat: implement solutions for min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
+exports.max = function max(num) {
+    if (!num || !num.length) {
+        return 0;
+    }
+    let maxNum = num[0];
+    for (let i = 1; i < num.length; i++) {
+        if (num[i] > maxNum) {
+            maxNum = num[i];
+        }
+    }
+    return maxNum;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(num) {
+    if (!num || !num.length) {
+        return 0;
+    }
+    let minNum = num[0];
+    for (let i = 1; i < num.length; i++) {
+        if (num[i] < minNum) {
+            minNum = num[i];
+        }
+    }
+    return minNum;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(num) {
+    if (!num || !num.length) {
+        return 0;
+    }
+    let sum = 0;
+    for (let i = 0; i < num.length; i++) {
+        sum += num[i];
+    }
+    return sum / num.length;
+};
